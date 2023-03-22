e9f8e137fbebe0d88b5e443718e1c74e1e1725b1
21496073 <103001539+21496073@users.noreply.github.com>
2023-03-22 14:56:47 +0200

    Add project files.

---
+++ Prac 2/Controllers/StudyGrpController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prac_2.Controllers
+{
+    public class StudyGrpController : Controller
+    {
+        // GET: StudyGrp
+        public ActionResult Index()
+        {
+            List<Models.myStudyGroupClass> MyGroup = new List<Models.myStudyGroupClass>();
+
+            MyGroup.Add(new Models.myStudyGroupClass { StudentNumber = "21496073", Name = "Jonny", Surname = "Cyns", EmailAddress = "u21496073@tuks.co.za" });
+            MyGroup.Add(new Models.myStudyGroupClass { StudentNumber = "21496088", Name = "Manny", Surname = "Paqman", EmailAddress = "u21496088@tuks.co.za" });
+            MyGroup.Add(new Models.myStudyGroupClass { StudentNumber = "21494473", Name = "Mthembza", Surname = "Goat", EmailAddress = "u21494473@tuks.co.za" });
+            MyGroup.Add(new Models.myStudyGroupClass { StudentNumber = "22496073", Name = "Malcolm", Surname = "Extra", EmailAddress = "u22496073@tuks.co.za" });
+            MyGroup.Add(new Models.myStudyGroupClass { StudentNumber = "20496073", Name = "Lungelo", Surname = "Mpanza", EmailAddress = "u20496073@tuks.co.za" });
+            
+
+            return View(MyGroup);
+        }
+
+       
+    }
+}
---
+++ Prac 2/Models/myStudyGroupClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prac_2.Models
+{
+    public class myStudyGroupClass
+    {
+
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Link")]
+        public string myLink { get; set; }
+
+    }
+}
